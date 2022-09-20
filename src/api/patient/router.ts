7c323cc182ca2a09b50be4f708ff26ccf3d2c833
João Denis <joao.denisr@gmail.com>
2022-09-19 21:28:12 -0300

    UPDATE: Swagger

--- src/api/patient/router.ts
+++ src/api/patient/router.ts
@@ -6,10 +6,114 @@ import { verifyBody, verifyQuery } from '../../utils/middlewares/validation';
 const router = Router();
 
 const patientRoutes = () => {
-    
+    /**
+     * @swagger
+     * components:
+     *   schemas:
+     *     Patient:
+     *       type: object
+     *       properties:
+     *         name:
+     *           type: string
+     *           description: The patient's name.
+     *           example: Leanne Graham
+     *         cpf:
+     *           type: string
+     *           description: Patient CPF
+     *           example: 000.000.000-00
+     *         email:
+     *           type: string
+     *           description: Patient email
+     *           example: mail@mail.com
+     *         password:
+     *           type: string
+     *           description: Password
+     *           example: myPassw0rd
+     *         birthDate:
+     *           type: string
+     *           description: Birthdate
+     *           example: 01/01/1990
+     *         gender:
+     *           type: number
+     *           description: 0 - MALE, 1 - FEMALE, 2 - NON_BINARY, 3 - OTHER
+     */
+
+    /**
+     * @swagger
+     * /api/v1/patient:
+     *   get:
+     *     summary: List patients
+     *     description: List patients.
+     *     tags: [Patient]
+    */
     router.get('', verifyQuery(validateFilter()), PatientController.list);
+    /**
+     * @swagger
+     * /api/v1/patient:
+     *   post:
+     *     summary: Create a patient
+     *     description: Create a patient.
+     *     tags: [Patient]
+     *     requestBody:
+     *        content:
+     *          application/json:
+     *            schema:
+     *              $ref: '#/components/schemas/Patient'
+     *     responses:
+     *       201:
+     *         description: Success
+     *         content:
+     *           application/json:
+     *             schema:
+     *              $ref: '#/components/schemas/Patient'
+     *       500:
+     *         description: Error
+    */
     router.post('', validateCreate(), verifyBody, PatientController.create);
+
+    /**
+     * @swagger
+     * /api/v1/patient/{id}:
+     *   patch:
+     *     summary: Update patient
+     *     description: Update patient.
+     *     tags: [Patient]
+     *     requestBody:
+     *        content:
+     *          application/json:
+     *            schema:
+     *              $ref: '#/components/schemas/Patient'
+     *     parameters:
+     *       - in: path
+     *         name: id
+     *         required: true
+     *         description: String ID of the user to retrieve.
+     *         schema:
+     *           type: string
+    */
     router.patch(':patientId', validateCreate(), verifyBody, PatientController.partialUpdate);
+
+    /**
+     * @swagger
+     * /api/v1/patient/{id}:
+     *   delete:
+     *     summary: Delete professional
+     *     description: Deactivate a professional.
+     *     tags: [Patient]
+     *     parameters:
+     *       - in: path
+     *         name: id
+     *         required: true
+     *         description: String ID of the user to retrieve.
+     *         schema:
+     *           type: string
+     *     responses:
+     *       204:
+     *         description: Success
+     *       404:
+     *         description: Not Found
+    */
+    router.delete('/:professionalId', PatientController.delete);
 
     return router;
 }
--- src/api/professional/router.ts
+++ src/api/professional/router.ts
@@ -6,7 +6,42 @@ import { verifyBody, verifyQuery } from '../../utils/middlewares/validation';
 const router = Router();
 
 const professionalRoutes = () => {
-    
+    /**
+     * @swagger
+     * components:
+     *   schemas:
+     *     Professional:
+     *       type: object
+     *       properties:
+     *         name:
+     *           type: string
+     *           description: The patient's name.
+     *           example: Leanne Graham
+     *         cpf:
+     *           type: string
+     *           description: Patient CPF
+     *           example: 000.000.000-00
+     *         email:
+     *           type: string
+     *           description: Patient email
+     *           example: mail@mail.com
+     *         password:
+     *           type: string
+     *           description: Password
+     *           example: myPassw0rd
+     *         birthDate:
+     *           type: string
+     *           description: Birthdate
+     *           example: 01/01/1990
+     *         gender:
+     *           type: number
+     *           description: 0 - MALE, 1 - FEMALE, 2 - NON_BINARY, 3 - OTHER
+     *         professionalIdentification:
+     *           type: string
+     *           description: Professional identification
+     *           example: CRP-UF-00000
+     */
+
     /**
      * @swagger
      * /api/v1/professional:
@@ -28,24 +63,52 @@ const professionalRoutes = () => {
      *        content:
      *          application/json:
      *            schema:
-     *              type: object
+     *              $ref: '#/components/schemas/Professional'
     */
     router.post('', validateCreate(), verifyBody, ProfessionalController.create);
 
     /**
      * @swagger
      * /api/v1/professionl/{id}:
-     *   post:
-     *     summary: Create new professional
-     *     description: Create new professional.
+     *   patch:
+     *     summary: Update professional
+     *     description: Update professional.
      *     tags: [Professional]
      *     requestBody:
      *        content:
      *          application/json:
      *            schema:
-     *              type: object
+     *              $ref: '#/components/schemas/Professional'
+     *     parameters:
+     *       - in: path
+     *         name: id
+     *         required: true
+     *         description: String ID of the user to retrieve.
+     *         schema:
+     *           type: string
     */
     router.patch('/:professionalId', validateCreate(), verifyBody, ProfessionalController.partialUpdate);
+
+    /**
+     * @swagger
+     * /api/v1/professionl/{id}:
+     *   delete:
+     *     summary: Delete professional
+     *     description: Deactivate a professional.
+     *     tags: [Professional]
+     *     parameters:
+     *       - in: path
+     *         name: id
+     *         required: true
+     *         description: String ID of the user to retrieve.
+     *         schema:
+     *           type: string
+     *     responses:
+     *       204:
+     *         description: Success
+     *       404:
+     *         description: Not Found
+    */
     router.delete('/:professionalId', ProfessionalController.delete);
 
     return router;
